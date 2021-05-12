f3047c6105f6d3fd2d9dd9aeb49fc9b6723f0b03
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-05-12 09:18:33 +0200

    clean up index.js

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
-// servidor de express
-const express = require('express');
-const app = express();
-const path = require('path');
+const Server = require('./models/server');
 
-require('dotenv').config();
-// servidor de sockets
-const server = require('http').createServer(app);
+const server = new Server();
 
-// configuración del socket server
-const io = require('socket.io')(server);
+server.execute();
 
-// desplegar el directorio publico
-app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connection', (socket) => {
-  socket.emit('mensaje-bienvenida', {
-    msg: 'Bienvenido al server',
-    fecha: new Date(),
-  });
-
-  socket.on('mensaje-cliente', (data) => {
-    console.log(data);
-  });
-});
-
-server.listen(process.env.PORT, () => {
-  console.log(`Server is running on ${process.env.PORT}`);
-});
