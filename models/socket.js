9dd494e3916755ddbe910d550a6f66988354dd43
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-05-16 14:37:02 +0200

    added increaseVotes and refresh new bands

--- models/socket.js
+++ models/socket.js
@@ -15,6 +15,14 @@ class Sockets {
 
       // emitir al cliente conectado, todas las bandas actuales
       socket.emit('current-bands', this.bandList.getBands());
+
+      // votar por la banda
+      socket.on('vote-band', (id) => {
+        this.bandList.increaseVotes(id);
+        // yo recibo votos entonces voy a emitir que hay nuevos votos
+        // esto cambio entonces hay que refrescarlo
+        this.io.emit('current-bands', this.bandList.getBands());
+      });
     });
   }
 }
